453a24c42edff77cb80b83d6dcea323e0871a157
Siluxsept <ronny.horn.rh@gmail.com>
2024-11-20 16:30:20 +0100

    b4s4: include PifPrebuilt.apk
    Change-Id: I895d08e67b14771aa25ae494c93ec90c4002a490

--- device-lineage.mk
+++ device-lineage.mk
@@ -10,6 +10,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# PifPrebuilt
+$(call inherit-product-if-exists, vendor/certification/config.mk)
+
 # AiAi Config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
